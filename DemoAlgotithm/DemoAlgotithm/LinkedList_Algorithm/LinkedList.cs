6e571dfd272ac2fc667a3b4c05d4f9e7378c7407
Thuan <carrie.he1704@gmail.com>
2017-02-16 16:06:06 +0700

    init

---
+++ DemoAlgotithm/DemoAlgotithm/LinkedList_Algorithm/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAlgotithm.LinkedList_Algorithm
+{
+    public class LinkedListNode<T>
+    {
+
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public LinkedListNode<T> Next { get; set; }
+
+        
+    }
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; set; }
+
+        public LinkedListNode<T> Tail { get; set; }
+
+        public void Add(LinkedListNode<T> value)
+        {
+            if (value != null)
+            {
+                if (Head == null)
+                {
+                    Head = value;
+                    Tail = value;
+                }
+                else
+                {
+                    Tail.Next = value;
+                    Tail = value;
+                }
+            }
+        }
+    }
+}
---
+++ DemoAlgotithm/DemoAlgotithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAlgotithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList_Algorithm.LinkedList<int> linkedList = new LinkedList_Algorithm.LinkedList<int>();
+            linkedList.Add(new LinkedList_Algorithm.LinkedListNode<int>(1));
+            linkedList.Add(new LinkedList_Algorithm.LinkedListNode<int>(2));
+            linkedList.Add(new LinkedList_Algorithm.LinkedListNode<int>(3));
+        }
+    }
+}
