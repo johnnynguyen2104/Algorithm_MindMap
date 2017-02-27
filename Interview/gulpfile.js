9c9061c5629aa390217721766fbdbc8806e1f1b0
Thuan <carrie.he1704@gmail.com>
2017-02-27 23:02:30 +0700

    update code

---
+++ Interview/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('watcher', function () {
+    gulp.watch('app/styles/*.scss', ['sass']);
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src('app/styles/*.scss')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('app/css'))
+});
