7135eabe68b374677cbf32d01deadad1a3271c34
Johnny Nguyen <johnny@goquo.com>
2018-09-20 18:09:13 +0800

    + maximumNonAdjacentCellsSum

--- DemoAlgotithm/DemoAlgotithm/Assignments/Matrix.cs
+++ DemoAlgotithm/DemoAlgotithm/Assignments/Matrix.cs
@@ -9,6 +9,183 @@ namespace DemoAlgotithm.Assignments
 {
     public partial class Assignments
     {
+        public static int maximumNonAdjacentCellsSum(int[,] matrix)
+        {
+            int row = matrix.GetLength(0)
+                , column = matrix.GetLength(1)
+                , max = int.MinValue;
+            List<string> array = new List<string>();     
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (matrix[i,j] > max)
+                    {
+                        array.Insert(0, $"{i}.{j}");
+                        max = matrix[i, j];
+                    }
+                    else
+                    {
+                        array.Add($"{i}.{j}");
+                    }
+                }
+            }
+
+            int c, d, c1, d1;
+            string[] splited , splited1;
+            array.Sort(delegate (string x, string y) {
+
+                splited = x.Split('.');
+                d = int.Parse(splited[0]);
+                c = int.Parse(splited[1]);
+
+                splited1 = y.Split('.');
+                d1 = int.Parse(splited1[0]);
+                c1 = int.Parse(splited1[1]);
+
+
+                return (matrix[d1, c1].CompareTo(matrix[d, c]));
+            });
+
+            int result = 0;
+            //use max + 1 as a marker
+            max++;
+            for (int i = 0; i < array.Count; i++)
+            {
+                splited = array[i].Split('.');
+                d = int.Parse(splited[0]);
+                c = int.Parse(splited[1]);
+                //top
+                if (c - 1 >= 0)
+                {
+                    if (matrix[d, c- 1] == max)
+                    {
+                        continue;
+                    }
+                }
+
+                //down
+                if (c + 1 <= column - 1)
+                {
+                    if (matrix[d, c + 1] == max)
+                    {
+                        continue;
+                    }
+                }
+                //Left
+                if (d - 1 >= 0)
+                {
+                    if (matrix[d - 1, c] == max)
+                    {
+                        continue;
+                    }
+                }
+                //Right
+                if (d + 1 <= row - 1)
+                {
+                    if (matrix[d + 1, c] == max)
+                    {
+                        continue;
+                    }
+                }
+
+                result += matrix[d, c];
+                matrix[d, c] = max;
+            }
+
+            return result;
+        }
+
+
+        public static int maximumNonAdjacentCellsSum(int[][] matrix)
+        {
+            int row = matrix.GetLength(0)
+               , column = matrix[0].Length
+               , max = int.MinValue;
+            List<string> array = new List<string>();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (matrix[i][j] > max)
+                    {
+                        array.Insert(0, $"{i}.{j}");
+                        max = matrix[i][j];
+                    }
+                    else
+                    {
+                        array.Add($"{i}.{j}");
+                    }
+                }
+            }
+
+            int c, d, c1, d1;
+            string[] splited, splited1;
+            array.Sort(delegate (string x, string y) {
+
+                splited = x.Split('.');
+                d = int.Parse(splited[0]);
+                c = int.Parse(splited[1]);
+
+                splited1 = y.Split('.');
+                d1 = int.Parse(splited1[0]);
+                c1 = int.Parse(splited1[1]);
+
+
+                return (matrix[d1][c1].CompareTo(matrix[d][c]));
+            });
+
+            int result = 0;
+            //use max + 1 as a marker
+            max++;
+            for (int i = 0; i < array.Count; i++)
+            {
+                splited = array[i].Split('.');
+                d = int.Parse(splited[0]);
+                c = int.Parse(splited[1]);
+                //top
+                if (c - 1 >= 0)
+                {
+                    if (matrix[d][c - 1] == max)
+                    {
+                        continue;
+                    }
+                }
+
+                //down
+                if (c + 1 <= column - 1)
+                {
+                    if (matrix[d][c + 1] == max)
+                    {
+                        continue;
+                    }
+                }
+                //Left
+                if (d - 1 >= 0)
+                {
+                    if (matrix[d - 1][c] == max)
+                    {
+                        continue;
+                    }
+                }
+                //Right
+                if (d + 1 <= row - 1)
+                {
+                    if (matrix[d + 1][c] == max)
+                    {
+                        continue;
+                    }
+                }
+
+                result += matrix[d][c];
+                matrix[d][c] = max;
+            }
+
+            return result;
+        }
+
 
         static int DiagonalDifference(int[][] a)
         {
--- DemoAlgotithm/DemoAlgotithm/Program.cs
+++ DemoAlgotithm/DemoAlgotithm/Program.cs
@@ -12,13 +12,17 @@ namespace DemoAlgotithm
         static void Main(string[] args)
         {
 
-            Console.WriteLine(TreeAssignment.hasPathWithGivenSum(new Tree<int>() {
-                left = new Tree<int>() { value = 1, left = new Tree<int>() { value = -2, right = new Tree<int>() { value = 3 } } },
-                right = new Tree<int>() { value = 3, left = new Tree<int>() { value = 1 }, right = new Tree<int>() { value = 2, left = new Tree<int>() { value = -2 }, right = new Tree<int>() { value = -3 } } },
-                value = 4
+            //Console.WriteLine(TreeAssignment.hasPathWithGivenSum(new Tree<int>() {
+            //    left = new Tree<int>() { value = 1, left = new Tree<int>() { value = -2, right = new Tree<int>() { value = 3 } } },
+            //    right = new Tree<int>() { value = 3, left = new Tree<int>() { value = 1 }, right = new Tree<int>() { value = 2, left = new Tree<int>() { value = -2 }, right = new Tree<int>() { value = -3 } } },
+            //    value = 4
 
-            }, 7));
-
+            //}, 7));
+            int[,] matrix = new int[,]{{5, 5, -1, 2 },
+ { 5, -1, 20, 15},
+ {5, 20, -2, 13},
+ {5, 10, 10, 30}};
+            var a = Assignments.Assignments.maximumNonAdjacentCellsSum(matrix);
             Console.ReadLine();
         }
     }
